46d9ddc8e373781f6bbaded6129424b867174aff
NeeeeeeaL <adam0618@outlook.com>
2019-11-20 21:35:39 +0800

    槽函数尝试

--- code/QtTest1/QtTest1/QtTest1.cpp
+++ code/QtTest1/QtTest1/QtTest1.cpp
@@ -1,5 +1,6 @@
 #include "QtTest1.h"
 #include <QDebug>
+#include <QMessageBox>
 #include <QTextCodec> //转换字符头文件
 
 QTextCodec *codec;
@@ -9,18 +10,19 @@ QtTest1::QtTest1(QWidget *parent)
 {
 	//给标签设置内容
 	ui.setupUi(this);
-	ui.label_2->setText("Hello Qt!");
+	ui.label_2->setText("+");
 	//给编辑区设置中文内容，会乱码
 	ui.textEdit->setText("Hello NeaL!");
 
+	ui.label_3->setText("=");
 
-	//给编辑区设置中文内容
-	//ui.label_3->setText("你好！");
-	//初始化codec
-	codec = QTextCodec::codecForName("GBK");
-	//转换
-	char *pstr = "你好！！！";
-	ui.label_3->setText(codec->toUnicode(pstr));
+	////给编辑区设置中文内容
+	////ui.label_3->setText("你好！");
+	////初始化codec
+	//codec = QTextCodec::codecForName("GBK");
+	////转换
+	//char *pstr = "你好！！！";
+	//ui.label_3->setText(codec->toUnicode(pstr));
 
 
 	//获取标签内容，返回值为QString
@@ -29,4 +31,20 @@ QtTest1::QtTest1(QWidget *parent)
 	qDebug() << "Output";
 }
 
+//spinBox加法实现
+void QtTest1::inPut1_ValueChanged(int value)
+{
+	ui.spinResult->setText(QString::number(value + ui.inPut2->value()));
+}
+
+void QtTest1::inPut2_ValueChanged(int value)
+{
+	ui.spinResult->setText(QString::number(value + ui.inPut1->value()));
+}
+
+//点击菜单NeaL
+void QtTest1::on_menuNeaL_clicked()
+{
+	QMessageBox();
+}
 
--- code/QtTest1/QtTest1/QtTest1.h
+++ code/QtTest1/QtTest1/QtTest1.h
@@ -12,4 +12,10 @@ public:
 
 private:
 	Ui::QtTest1Class ui;
+
+	private slots:
+	void inPut1_ValueChanged(int value);
+	void inPut2_ValueChanged(int value);
+	void on_menuNeaL_clicked();
+
 };
